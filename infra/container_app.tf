e4a7cc1b4cf14014a1da56c2f22f5bef7dafe5c4
Hien Vuong <hien.vuong@codeleap.de>
2026-03-26 22:39:34 +0700

    provision resources for backend

--- infra/container_app.tf
+++ infra/container_app.tf
@@ -1,81 +1,71 @@
-# resource "azurerm_container_app_environment" "cae" {
-#   name                       = "cae-${local.project_name}-${var.environment}"
-#   resource_group_name        = azurerm_resource_group.rg.name
-#   location                   = azurerm_resource_group.rg.location
-#   logs_destination           = "log-analytics"
-#   log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
-# }
+resource "azurerm_container_app_environment" "cae" {
+  name                       = "cae-${local.project_name}"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  logs_destination           = "log-analytics"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
+}
 
-# resource "azurerm_container_app" "ca" {
-#   name                         = "ca-${local.project_name}-${var.environment}"
-#   container_app_environment_id = azurerm_container_app_environment.cae.id
-#   resource_group_name          = azurerm_container_app_environment.cae.resource_group_name
-#   revision_mode                = "Single"
+resource "azurerm_container_app" "ca" {
+  name                         = "ca-${local.project_name}"
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  resource_group_name          = azurerm_container_app_environment.cae.resource_group_name
+  revision_mode                = "Single"
 
-#   registry {
-#     server   = azurerm_container_registry.acr.login_server
-#     identity = azurerm_user_assigned_identity.id.id
-#   }
+  registry {
+    server   = azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.id.id
+  }
 
-#   template {
-#     container {
-#       name   = "examplecontainerapp"
-#       image  = "mcr.microsoft.com/k8se/quickstart:latest"
-#       cpu    = 0.25
-#       memory = "0.5Gi"
-#     }
-#     cooldown_period_in_seconds = 900 # 15 minutes
-#   }
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+    cooldown_period_in_seconds = 300
+  }
 
-#   ingress {
-#     target_port      = 8457
-#     external_enabled = true
-#     traffic_weight {
-#       latest_revision = true
-#       percentage      = 100
-#     }
-#   }
+  ingress {
+    target_port      = 8082
+    external_enabled = true
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
 
-#   identity {
-#     type         = "UserAssigned"
-#     identity_ids = [azurerm_user_assigned_identity.id.id]
-#   }
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.id.id]
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       template[0].container
-#     ]
-#   }
+  lifecycle {
+    ignore_changes = [
+      template[0].container
+    ]
+  }
 
-#   secret {
-#     name                = "azure-openai-api-key"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.azure-openai-api-key.versionless_id
-#   }
-#   secret {
-#     name                = "jwt-secret-key"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.jwt-secret-key.versionless_id
-#   }
-#   secret {
-#     name                = "database-url"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.database-url.versionless_id
-#   }
-#   secret {
-#     name                = "communication-service-connection-string"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.communication-service-connection-string.versionless_id
-#   }
-#   secret {
-#     name                = "storage-account-connection-string"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.storage-account-connection-string.versionless_id
-#   }
-#   secret {
-#     name                = "azure-openai-api-key-fallback"
-#     identity            = azurerm_user_assigned_identity.id.id
-#     key_vault_secret_id = azurerm_key_vault_secret.azure-openai-api-key-fallback.versionless_id
-#   }
-#   depends_on = [azurerm_role_assignment.key_vault_id]
-# }
+  secret {
+    name                = "mysql-database-name"
+    identity            = azurerm_user_assigned_identity.id.id
+    key_vault_secret_id = azurerm_key_vault_secret.mysql-database-name.versionless_id
+  }
+  secret {
+    name                = "mysql-url"
+    identity            = azurerm_user_assigned_identity.id.id
+    key_vault_secret_id = azurerm_key_vault_secret.mysql-url.versionless_id
+  }
+  secret {
+    name                = "mysql-username"
+    identity            = azurerm_user_assigned_identity.id.id
+    key_vault_secret_id = azurerm_key_vault_secret.mysql-username.versionless_id
+  }
+  secret {
+    name                = "mysql-root-password"
+    identity            = azurerm_user_assigned_identity.id.id
+    key_vault_secret_id = azurerm_key_vault_secret.mysql-root-password.versionless_id
+  }
+  depends_on = [azurerm_role_assignment.key_vault_id]
+}
--- infra/keyvault.tf
+++ infra/keyvault.tf
@@ -1,45 +1,33 @@
-# resource "azurerm_key_vault" "kv" {
-#   name                       = var.environment == "prod" ? "keyvault-${local.project_name}-${var.environment}" : "kv-${local.project_name}-${var.environment}"
-#   location                   = azurerm_resource_group.rg.location
-#   resource_group_name        = azurerm_resource_group.rg.name
-#   tenant_id                  = data.azurerm_client_config.current.tenant_id
-#   sku_name                   = "standard"
-#   purge_protection_enabled   = true
-#   rbac_authorization_enabled = true
-# }
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-${local.project_name}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  purge_protection_enabled   = true
+  rbac_authorization_enabled = true
+}
 
-# resource "azurerm_key_vault_secret" "jwt-secret-key" {
-#   name         = "jwt-secret-key"
-#   value        = var.jwt_secret_key
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
+resource "azurerm_key_vault_secret" "mysql-database-name" {
+  name         = "mysql-database-name"
+  value        = azurerm_mysql_flexible_database.mysql_db.name
+  key_vault_id = azurerm_key_vault.kv.id
+}
 
-# resource "azurerm_key_vault_secret" "communication-service-connection-string" {
-#   name         = "communication-service-connection-string"
-#   value        = azurerm_communication_service.acs.primary_connection_string
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
+resource "azurerm_key_vault_secret" "mysql-url" {
+  name         = "mysql-url"
+  value        = azurerm_mysql_flexible_server.mysql_server.fqdn
+  key_vault_id = azurerm_key_vault.kv.id
+}
 
-# resource "azurerm_key_vault_secret" "azure-openai-api-key" {
-#   name         = "azure-openai-api-key"
-#   value        = azurerm_cognitive_account.aif.primary_access_key
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
+resource "azurerm_key_vault_secret" "mysql-username" {
+  name         = "mysql-username"
+  value        = azurerm_mysql_flexible_server.mysql_server.administrator_login
+  key_vault_id = azurerm_key_vault.kv.id
+}
 
-# resource "azurerm_key_vault_secret" "database-url" {
-#   name         = "database-url"
-#   value        = "postgresql+asyncpg://${azurerm_postgresql_flexible_server.psqlserver.administrator_login}:${urlencode(azurerm_postgresql_flexible_server.psqlserver.administrator_password)}@${azurerm_postgresql_flexible_server.psqlserver.name}.postgres.database.azure.com:5432/${azurerm_postgresql_flexible_server_database.psql.name}"
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
-
-# resource "azurerm_key_vault_secret" "storage-account-connection-string" {
-#   name         = "storage-account-connection-string"
-#   value        = azurerm_storage_account.st.primary_connection_string
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
-
-# resource "azurerm_key_vault_secret" "azure-openai-api-key-fallback" {
-#   name         = "azure-openai-api-key-fallback"
-#   value        = azurerm_cognitive_account.aif_fallback.primary_access_key
-#   key_vault_id = azurerm_key_vault.kv.id
-# }
+resource "azurerm_key_vault_secret" "mysql-root-password" {
+  name         = "mysql-root-password"
+  value        = azurerm_mysql_flexible_server.mysql_server.administrator_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
