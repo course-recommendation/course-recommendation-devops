5957602abdaf139a89127ab5210a7e8633418db4
Hien Vuong <hien.vuong@codeleap.de>
2026-04-26 02:25:02 +0700

    upgrade github terraform

--- infra/github.tf
+++ infra/github.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "stapp_api_token" {
   repository      = "course-recommendation-react"
   secret_name     = "AZURE_STATIC_WEB_APPS_API_TOKEN"
-  plaintext_value = azurerm_static_web_app.stapp.api_key
+  value           = azurerm_static_web_app.stapp.api_key
 }
