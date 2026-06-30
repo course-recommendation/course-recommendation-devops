f249966de57825fc6dd029097de412beadbdb5ca
Hien Vuong <hienvuongnhat@gmail.com>
2026-07-01 00:05:39 +0700

    upgrade sku to standard

--- infra/static_web_app.tf
+++ infra/static_web_app.tf
@@ -2,7 +2,7 @@ resource "azurerm_static_web_app" "stapp" {
   name                = "stapp-${local.project_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku_tier            = "Standard"
+  sku_size           = "Standard"
   lifecycle {
     ignore_changes = [
       repository_branch,
